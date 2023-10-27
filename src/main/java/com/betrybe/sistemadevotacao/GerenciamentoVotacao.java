d82d9a8b48d12788fd2364f8287f4b9d3a079916
Ruan-Portella <ruanmorales29@gmail.com>
2023-10-27 14:28:49 -0300

    Feat: Requisito 04 Concluído

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,8 +1,12 @@
 package com.betrybe.sistemadevotacao;
 
+import java.util.ArrayList;
+
 /**
  * javadoc.
  */
-public class GerenciamentoVotacao {
-
+public abstract class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
+  ArrayList<String> pessoasCandidatas = new ArrayList<String>();
+  ArrayList<String> pessoasEleitoras = new ArrayList<String>();
+  ArrayList<String> cpfsComputados = new ArrayList<String>();
 }
