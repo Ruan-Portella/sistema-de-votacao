8fc3e9fb01806bd6aff10ceb0c670b472d1459d1
Ruan-Portella <ruanmorales29@gmail.com>
2023-10-27 14:14:02 -0300

    Feat: Requisito 02 Concluído

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,28 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaCandidata {
+public class PessoaCandidata extends Pessoa {
+    public int numeros;
+    public int votos;
 
+    public PessoaCandidata(String nome, int numeros) {
+        super.nome = nome;
+        this.numeros = numeros;
+        this.votos = 0;
+    }
+
+    public int getNumeros() {
+        return numeros;
+    }
+
+    public int getVotos() {
+        return votos;
+    }
+
+    public void setNumeros(int numeros) {
+        this.numeros = numeros;
+    }
+
+    public void receberVoto() {
+        this.votos++;
+    }
 }
