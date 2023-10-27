a2c2640a7703524942ec1cdb2443e28861cd851f
Ruan-Portella <ruanmorales29@gmail.com>
2023-10-27 14:43:17 -0300

    Feat: Requisito 05 Concluído

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -6,7 +6,35 @@ import java.util.ArrayList;
  * javadoc.
  */
 public abstract class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
-  ArrayList<String> pessoasCandidatas = new ArrayList<String>();
-  ArrayList<String> pessoasEleitoras = new ArrayList<String>();
-  ArrayList<String> cpfsComputados = new ArrayList<String>();
+  ArrayList<PessoaCandidata> pessoasCandidatas = new ArrayList<>();
+  ArrayList<PessoaEleitora> pessoasEleitoras = new ArrayList<>();
+  ArrayList<String> cpfsComputados = new ArrayList<>();
+
+  /**  
+   * Getters and Setters.
+   */
+
+  public void cadastrarPessoaCandidata(String nome, int numero) {
+    for (PessoaCandidata pessoaCandidata : pessoasCandidatas) {
+      if (pessoaCandidata.numeros == numero) {
+        System.out.println("Número da pessoa candidata já utilizado!");
+        return;
+       } else {
+         PessoaCandidata novaPessoaCandidata = new PessoaCandidata(nome, numero);
+         pessoasCandidatas.add(novaPessoaCandidata);
+       }
+     }
+   }
+
+  public void cadastrarPessoaEleitora(String nome, String cpf) {
+    for (PessoaEleitora pessoaEleitora : pessoasEleitoras) {
+      if (pessoaEleitora.getCpf() == cpf) {
+        System.out.println("Pessoa eleitora já cadastrada!");
+        return;
+       } else {
+         PessoaEleitora novaPessoaEleitora = new PessoaEleitora(nome, cpf);
+         pessoasEleitoras.add(novaPessoaEleitora);
+       }
+     }
+   }
 }
--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -1,21 +1,27 @@
 package com.betrybe.sistemadevotacao;
 
 /**
- * javadoc.
- */
-public class PessoaEleitora extends Pessoa{
+  * javadoc.
+  */
+
+public class PessoaEleitora extends Pessoa {
   private String cpf;
 
+  
   public PessoaEleitora(String cpf, String nome) {
     super.nome = nome;
     this.cpf = cpf;
   }
-
-  public String getCPF() {
+  
+  /**
+    * Getters and Setters.
+    */
+  
+  public String getCpf() {
     return cpf;
   }
 
-  public void setCPF(String cpf) {
-     this.cpf = cpf;
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
   }
 }
