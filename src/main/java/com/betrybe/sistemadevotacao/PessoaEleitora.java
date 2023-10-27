52e83a19bca9498baa28211e9a2242dd86fbb117
Ruan-Portella <ruanmorales29@gmail.com>
2023-10-27 14:26:12 -0300

    Feat: Requisito 03 Concluído

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -3,6 +3,19 @@ package com.betrybe.sistemadevotacao;
 /**
  * javadoc.
  */
-public class PessoaEleitora{
+public class PessoaEleitora extends Pessoa{
+  private String cpf;
 
+  public PessoaEleitora(String cpf, String nome) {
+    super.nome = nome;
+    this.cpf = cpf;
+  }
+
+  public String getCPF() {
+    return cpf;
+  }
+
+  public void setCPF(String cpf) {
+     this.cpf = cpf;
+  }
 }
